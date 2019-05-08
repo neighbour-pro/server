3bb245c33e1f197219b0748d2af61e06bff26306
Raúl Contreras <raulc98@gmail.com>
2019-05-08 11:23:54 +0200

    Now you can get one professional by id

--- routes/professional.js
+++ routes/professional.js
@@ -7,11 +7,24 @@ router.get('/all', (req, res, next) => {
     .then(professionals => {
       res.status(200).json({
         data: professionals
-      })
+      });
     })
     .catch(err => res.status(500).json({
       message: 'Error retrieving the data',
       error: err
+    }));
+});
+
+router.get('/:id', (req, res, next) => {
+  Professional.findById(req.params.id)
+    .then(professional => {
+      res.status(200).json({
+        data: professional
+      });
+    })
+    .catch(err => res.status(500).json({
+      message: 'Error retrieving the data',
+      error: err,
     }));
 });
 
