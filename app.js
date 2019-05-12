505811cff23b7dfae0ee2f31a4faa5015813a61a
Raul Contreras <raulc98@gmail.com>
2019-05-12 16:04:12 +0200

    Added deployment route to the cors configuration

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000']
+  origin: ['http://localhost:3000', 'http://neighbour-pro.herokuapp.com']
 }));
 
 // Enable authentication using session + passport
