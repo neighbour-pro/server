bd36ef7f9d713e603a6cc1663eb9d9f2d2c09ae2
Raul Contreras <raulc98@gmail.com>
2019-05-12 17:59:15 +0200

    Now you can create a conversation and add new messages to it

--- routes/conversation.js
+++ routes/conversation.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
 const Conversation = require('../models/Conversation');
+const Message = require('../models/Message');
 
 router.get('/:id', (req, res, next) => {
-  Conversation.find({
+  Conversation.findOne({
     $or: [
       {
         client_id: req.params.id
@@ -14,6 +14,7 @@ router.get('/:id', (req, res, next) => {
       }
     ]
   })
+  .populate('messages')
   .then(conversations => {
     res.status(200).json({
       conversations
@@ -24,5 +25,36 @@ router.get('/:id', (req, res, next) => {
     error: err
   }));
 });
+
+router.post('/add/:fromId/:toId', (req, res, next) => {
+  Conversation.create({
+    client_id: req.params.fromId,
+    professional_id: req.params.toId,
+    messages: []
+  })
+  .then(conversation => {
+    res.status(200).json(conversation)
+  })
+  .catch(err => res.status(500).json({
+    error:err
+  }));
+});
+
+router.post('/:id/addMessage', (req, res, next) => {
+  const message = new Message({
+    text: req.body.text,
+    user_id: req.body.userId
+  });
+  message.save()
+  .then(message => Conversation.findByIdAndUpdate(req.params.id, {
+    $push: {
+      messages: message
+    }
+  }, {new: true}))
+  .then(conversation => res.status(200).json(conversation))
+  .catch(err => res.status(500).json({error: err}));
+});
+
+
 
 module.exports = router;
